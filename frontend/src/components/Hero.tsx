bbc5e7ad5f1e82195338a692de0b6c4dc931264f
AgonHamiti <ah51397@ubt-uni.net>
2024-11-15 03:19:26 +0100

    first commit

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+    return (
+      <div className="bg-blue-800 pb-16">
+        <div className="container mx-auto flex flex-col gap-2">
+          <h1 className="text-5xl text-white font-bold">Discover your ideal stay</h1>
+          <p className="text-2xl text-white">
+          Uncover unbeatable hotel prices for your dream escape...
+          </p>
+        </div>
+      </div>
+  );
+};
+
+export default Hero;
---
+++ frontend/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// Export the configuration object
+export default defineConfig({
+  plugins: [react()],
+});
